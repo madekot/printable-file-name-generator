f7ff495501c43eee5b38f50a9abf1be9d2d6bd6f
Mikhail Golubtsov <mgolubcov@gmail.com>
2025-02-21 19:15:04 +0300

    feat: добавить target="_blank" с rel="noopener noreferrer" для безопасного открытия ссылки в новой вкладке
    chore: добавлена папка dist в .gitignore

--- src/Logo/Logo.tsx
+++ src/Logo/Logo.tsx
@@ -6,7 +6,12 @@ interface Props {
 
 export const Logo = ({ className }: Props) => {
   return (
-    <a className={clsx(className)} href={"https://t.me/+IvpqDKzVkpdjMWEy"}>
+    <a
+      className={clsx(className)}
+      href={"https://t.me/+IvpqDKzVkpdjMWEy"}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img
         src="/Mishka-I-Shishka-production.png"
         width={100}
