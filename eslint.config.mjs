853309be3c9bfe00044333621f07a811da4ccb0b
Mikhail Golubtsov <mgolubcov@gmail.com>
2025-04-07 13:44:36 +0300

    feat: подключил eslint-plugin-fsd-import и добавил правила для контроля импорта в FSD

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import reactHooks from 'eslint-plugin-react-hooks';
 import storybook from 'eslint-plugin-storybook'
+import fsdImport from "eslint-plugin-fsd-import";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -16,10 +17,16 @@ export default [
   ...storybook.configs['flat/recommended'],
   {
     languageOptions: { globals: globals.browser },
+    plugins: {
+      "fsd-import": fsdImport,
+    },
     rules: {
       "react/prop-types": "off",
       "no-unused-vars": "off",
       "no-undef": "off",
+      "fsd-import/fsd-relative-path": "error",
+      "fsd-import/public-api-imports": "error",
+      "fsd-import/layer-imports": "error",
     },
     settings: {
       react: {
